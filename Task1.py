a02c267e882f5a3675b616333b6b60abca220592
yichen123 <chenyi330@hotmail.com>
2019-04-25 07:46:01 +0800

    init

---
+++ Task1.py
@@ -0,0 +1,42 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+
+"""
+TASK 1:
+How many different telephone numbers are there in the records?
+Print a message:
+"There are <count> different telephone numbers in the records."
+"""
+
+met = []
+count = 0
+for entry in calls:
+    num1, num2 = entry[0], entry[1]
+    if num1 not in met:
+        met.append(num1)
+        count += 1
+    if num2 not in met:
+        met.append(num2)
+        count += 1
+
+for entry in texts:
+    num1, num2 = entry[0], entry[1]
+    if num1 not in met:
+        met.append(num1)
+        count += 1
+    if num2 not in met:
+        met.append(num2)
+        count += 1
+
+print("There are " + str(count) + " different telephone numbers in the records")
---
+++ Task2.py
@@ -0,0 +1,43 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+"""
+TASK 2: Which telephone number spent the longest time on the phone
+during the period? Don't forget that time spent answering a call is
+also time spent on the phone.
+Print a message:
+"<telephone number> spent the longest time, <total time> seconds, on the phone during
+September 2016.".
+"""
+def update(num, time, length):
+    if num not in time:
+        time[num] = length
+    else:
+        time[num] += length
+
+time = {}
+for num1, num2, _, length in calls:
+    length = int(length)
+    update(num1, time, length)
+    update(num2, time, length)
+
+longest = 0
+longest_num = None
+
+for num in time.keys():
+    if time[num] > longest:
+        longest = time[num]
+        longest_num = num
+
+if longest_num:
+    print(longest_num + ' spent the longest time, ' + str(longest) + ' seconds, on the phone during September 2016.')
