dd6e5277791befa190e66287dcca060257207284
Chen YI <chenyi330@hotmail.com>
2019-04-25 15:46:04 +0800

    Update Task4.py
    task4 done

--- Task4.py
+++ Task4.py
@@ -25,3 +25,35 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+
+
+def lexSort(list):
+    if list:
+        list.sort()
+    return list
+
+telemarketers = []
+nums = {'call_out':[], 'call_in':[], 'text_out':[], 'text_in':[]}
+
+for num1, num2, _, _ in calls:
+    if num1 not in nums['call_out']:
+        nums['call_out'].append(num1)
+    if num2 not in nums['call_in']:
+        nums['call_in'].append(num2)
+
+for num1, num2, _ in texts:
+    if num1 not in nums['text_out']:
+        nums['text_out'].append(num1)
+    if num2 not in nums['text_in']:
+        nums['text_in'].append(num2)
+
+for num in nums['call_out']:
+    if num not in nums['call_in'] and num not in nums['text_in'] and num not in nums['text_out']:
+        telemarketers.append(num)
+
+
+sorted = lexSort(telemarketers)
+print("These numbers could be telemarketers: ")
+if sorted:
+    for num in sorted:
+        print(num)
